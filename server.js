8062f9d036146291d807a557752be0d4f1ca0bf7
zoomzoom6 <tonyhuang107@live.ca>
2021-06-22 16:44:44 -0400

    populate Animals page with all entries

--- server.js
+++ server.js
@@ -114,7 +114,23 @@ app.get('/api/animals/:id', (req, res) => {
     }
 });
 
+//connects to main/index html file
 app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+//connects to animals html file
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+//connects to zookeepers html file
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+//return this if user enters url to site that doesn't exist (ex. About, Contact, etc)
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
